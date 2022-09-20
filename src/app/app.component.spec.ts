ed4309acf683a758db2b872c72a1be982b6d6e14
jairH3110 <chiefeon3110@gmail.com>
2022-09-20 13:35:51 -0500

    rompiendo repo #1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent Test suit', () => {
   it(`should have as title 'unit-testing'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('unit-testing');
+    expect(app.title).toEqual('unit-testingd');
   });
 
   it('should render title', () => {
