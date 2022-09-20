03fe71ab6eeb3e36c2f56bcff221441b19c0096c
jairH3110 <chiefeon3110@gmail.com>
2022-09-19 21:25:08 -0500

    unit testing #1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
+describe('AppComponent Test suit', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
