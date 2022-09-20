951ffbbc6229b682ab18743497f229392c1ba266
jairH3110 <chiefeon3110@gmail.com>
2022-09-20 13:45:23 -0500

    problema resuelto #1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent Test suit', () => {
   it(`should have as title 'unit-testing'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('unit-testingd');
+    expect(app.title).toEqual('unit-testing');
   });
 
   it('should render title', () => {
