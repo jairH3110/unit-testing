cdcfab5ed34d74ed125f01df4c41e6b6bccb1557
jairH3110 <chiefeon3110@gmail.com>
2022-10-10 16:20:44 -0500

    añadiendo pow

---
+++ src/app/04-pow/pow.spec.ts
@@ -0,0 +1,22 @@
+import {pow } from './pow';
+
+describe('pow',()=>{
+    it('Should return 9 if x=3, n=2',() => {
+       const result = pow(2,3);
+        expect(result).toBe(8);
+    })
+})
+
+describe('pow',()=>{
+    it('Should return 9 if x=2, n=3',() => {
+       const result = pow(3,2);
+        expect(result).toBe(3);
+    })
+})
+
+describe('pow',()=>{
+    it('Should return 9 if  n=0',() => {
+       const result = pow(0,3);
+        expect(result).toBe(0);
+    })
+})
